f2930b83508f12b924794199c5799ad6532a39e7
sk784 <44239544+sk784@users.noreply.github.com>
2020-06-12 15:06:01 +0300

    Update movie_repository.dart
    внес правки

--- cinema_db_bloc/lib/repository/movie_repository.dart
+++ cinema_db_bloc/lib/repository/movie_repository.dart
@@ -5,12 +5,11 @@ class MovieRepository {
   final _api = ApiMovies.instance;
 
   Future getMoviesFromApi() async {
-    await _api.fetchMovies().then ((data) {
-     for (int i = 0; i < data.length; i++) {
-        DatabaseProvider.db.addMovie(data[i]);
-      }
-    });
-    return _api.fetchMovies();
+    List<Movie> result = await _api.fetchMovies();
+    for (int i = 0; i < result.length; i++) {
+      DatabaseProvider.db.addMovie(result[i]);
+      return result;
+    }
   }
 
   Future getMoviesFromDB() async => await DatabaseProvider.db.getMovies();
